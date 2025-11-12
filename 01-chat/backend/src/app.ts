75fc02d93fdf42c8f366e13d8758623594ab79ad
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-11-12 16:57:36 +0100

    GenAI einfacher Chat und Strukturierter Content

--- 01-chat/backend/src/app.ts
+++ 01-chat/backend/src/app.ts
@@ -1,31 +1,41 @@
 import cors from 'cors';
 import type { ErrorRequestHandler } from 'express';
 import express from 'express';
-
 import mongoose from 'mongoose';
 import { OpenAI } from 'openai';
 import { zodResponseFormat } from 'openai/helpers/zod.js';
 import { z } from 'zod';
 
+// Gehört natürlich in eigene Module :)
 await mongoose.connect(process.env.MONGO_URI!, { dbName: 'chat' });
 
-const Chat = mongoose.model(
-  'chat',
-  new mongoose.Schema({
-    history: {
-      type: [Object],
-      default: [],
-    },
-  })
-);
+// Einfaches Schema, um Chatverlauf zu speichern
+type ChatMessage = OpenAI.Chat.Completions.ChatCompletionMessage;
 
+interface ChatDocument extends mongoose.Document {
+  history: ChatMessage[];
+}
+
+const chatSchema = new mongoose.Schema<ChatDocument>({
+  history: {
+    type: [Object],
+    default: [],
+  },
+});
+
+const Chat = mongoose.model<ChatDocument>('chat', chatSchema);
+
+// OpenAI Klasse ermöglicht client für die Verbindung mit verschiedensten AI-Providern
+// Google Gemini
 const client = new OpenAI({
   apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
   baseURL: 'https://generativelanguage.googleapis.com/v1beta/openai/',
 });
 
+// OpenAI ChatGPT
 // const client = new OpenAI();
 
+// Lokales Modell mit Ollama
 // const client = new OpenAI({
 //   baseURL: 'http://127.0.0.1:11434/v1',
 // });
@@ -44,35 +54,39 @@ app.get('/', (_req, res) => {
 app.post('/messages', async (req, res) => {
   const { prompt, chatId } = req.body;
 
-  let chat;
+  let chat: ChatDocument;
   if (!chatId) {
     chat = await Chat.create({ history: [] });
   } else {
-    chat = await Chat.findById(chatId);
+    chat = (await Chat.findById(chatId)) as ChatDocument;
   }
 
   const result = await client.chat.completions.create({
     // model: 'gpt-5-mini',
     // model: 'llama3.2',
     model: 'gemini-2.5-flash',
-    messages: [...chat?.history, { role: 'user', content: prompt }],
+    messages: [...chat.history, { role: 'user', content: prompt }],
   });
 
-  const answer = result.choices[0]?.message;
-  chat.history = [...chat.history, { role: 'user', content: prompt }, answer];
+  if (!result.choices[0]) throw new Error('Chat failed');
+
+  const answer = result.choices[0].message;
+  chat.history = [...chat.history, { role: 'user', content: prompt } as unknown as ChatMessage, answer];
   await chat.save();
 
-  res.json({ result: answer?.content, chatId: chat._id });
+  res.json({ result: answer.content, chatId: chat._id });
 });
 
 app.post('/images', async (req, res) => {
   const { prompt } = req.body;
 
   const result = await client.images.generate({
-    model: 'gemini-2.5-flash-image',
+    model: 'imagen-4.0-generate-001',
     prompt,
     response_format: 'b64_json',
   });
+
+  // const result = await client.models.list();
 
   res.json({ result });
 });
