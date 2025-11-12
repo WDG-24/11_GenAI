8c78207fb162c5d61144a818953e757dfc6cc8cc
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-11-12 11:50:27 +0100

    GenAI starter + ts

--- 01-chat/backend/src/app.ts
+++ 01-chat/backend/src/app.ts
@@ -1,9 +1,10 @@
 import cors from 'cors';
+import type { ErrorRequestHandler } from 'express';
 import express from 'express';
 
 import mongoose from 'mongoose';
 
-await mongoose.connect(process.env.MONGO_URI, { dbName: 'chat' });
+await mongoose.connect(process.env.MONGO_URI!, { dbName: 'chat' });
 
 const port = process.env.PORT || 8080;
 
@@ -20,9 +21,9 @@ app.use('/{*splat}', () => {
   throw Error('Page not found', { cause: { status: 404 } });
 });
 
-app.use((err, _req, res, _next) => {
+app.use(((err, _req, res, _next) => {
   console.log(err);
   res.status(err.cause?.status || 500).json({ message: err.message });
-});
+}) satisfies ErrorRequestHandler);
 
 app.listen(port, () => console.log(`AI Proxy listening on port ${port}`));
